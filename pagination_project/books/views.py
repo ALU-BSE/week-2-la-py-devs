efc3722eb0f638d8f9da7cd60ca328db427a45fe
Mpatrick12 <p.mukunzi@alustudent.com>
2025-05-20 10:19:00 +0200

    update

--- pagination_project/books/views.py
+++ pagination_project/books/views.py
@@ -5,7 +5,7 @@ from .models import Book
 
 def book_list(request):
     books = Book.objects.all().order_by('title')
-    paginator = Paginator(books, 5)
+    paginator = Paginator(books, 5)  # 5 bookr page
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, 'books/book_list.html', {'page_obj': page_obj})
